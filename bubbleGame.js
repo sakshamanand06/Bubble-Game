6cd5971bf6f52b3e069858e2849c28f2f0ffa3a5
Saksham Anand <saksham261261cp@gmail.com>
2025-02-22 19:13:19 +0530

    Make the site responsive and fix number to hit not appearing in bubbles sometimes

--- bubbleGame.js
+++ bubbleGame.js
@@ -1,43 +1,74 @@
-function makeBubble(state) {
+// state is game state, whether playing(1) or game over(0), num is number of bubbles
+function makeBubble(state, num) {
     let clutter = "";
+    let arr = [];
     if(state){
-        for(let i=0; i<152; i++){
+        for(let i=0; i<num; i++){
             let random = Math.ceil(Math.random()*20);
+            arr.push(random); // store the random numbers in array
             clutter += `<div class="bubble">${random}</div>`;
         }
         document.querySelector(".bottom").innerHTML = clutter;
-        document.querySelector(".hit").textContent = Math.ceil(Math.random()*20);
+        // the number to click is selected randomly from one of the bubbles, rather than generating a new random number which might or might not be in the bubbles
+        document.querySelector(".hit").textContent = arr[Math.floor(Math.random() * num)];
     }
-    else document.querySelector(".bottom").innerHTML = `<div style="width: 300px; height: 80px; font-size: 3rem; background-color:#DC143C; position:absolute;top:50%; text-align:center; border-radius: 8px;">Game Over</div> <div class="redo" style="width: 100px; height: 40px; font-size: 1.5rem; background-color:#DD6A00; position:absolute;top:85%;left:85%; text-align:center; border-radius: 8px; cursor: pointer">Reset</div>`;
+    else document.querySelector(".bottom").innerHTML = `<div id="over">Game Over</div><div id="replay">Reset</div>`;
 }
+
+// Timer logic and game over after time is up
 let timer = document.querySelector(".timer");
 setInterval(() => {
     let score = document.querySelector(".score").textContent;
     if(timer.textContent>0 && score>=0) timer.textContent--;
     if(timer.textContent==0){
-        makeBubble(0);
-        document.querySelector(".redo").addEventListener("click", reset);
+        makeBubble(0, bubbles);
+        document.querySelector("#replay").addEventListener("click", () => {reset(bubbles)});
     }
 }, 1000);
+
+// Check for valid bubble click and update the score
 document.querySelector(".bottom").addEventListener("click", (e) => {
     let hit = document.querySelector(".hit").textContent;
     if (e.target.classList.contains("bubble")){
         if(e.target.textContent == hit){
             document.querySelector(".score").textContent++;
-            makeBubble(1);
+            makeBubble(1, bubbles);
         }
         else document.querySelector(".score").textContent--;
         if(document.querySelector(".score").textContent<0){
-            makeBubble(0);
-            document.querySelector(".redo").addEventListener("click", reset);
+            makeBubble(0, bubbles);
+            document.querySelector("#replay").addEventListener("click", () => {reset(bubbles)});
         }
     }
 });
 
-function reset(){
-document.querySelector(".score").textContent = 0;
-document.querySelector(".timer").textContent = 60;
-makeBubble(1);
+// Start/restart the game
+function reset(num){
+    document.querySelector(".score").textContent = 0;
+    document.querySelector(".timer").textContent = 60;
+    makeBubble(1, num);
 }
 
-reset();
+// Calculating number of bubbles required according to window size
+
+let padd = parseFloat(getComputedStyle(document.querySelector(".bottom")).padding);
+let marg = parseFloat(getComputedStyle(document.querySelector(".bubble")).margin);
+
+let bottomWidth = parseFloat(getComputedStyle(document.querySelector(".bottom")).width);
+let bottomHeight = parseFloat(getComputedStyle(document.querySelector(".bottom")).height);
+
+let bubbleWidth = parseFloat(getComputedStyle(document.querySelector(".bubble")).width);
+let bubbleHeight = parseFloat(getComputedStyle(document.querySelector(".bubble")).height);
+
+let colno = Math.floor((bottomWidth - (2*padd))/(bubbleWidth + (2*marg)));
+let rowno = Math.floor((bottomHeight - (2*padd))/(bubbleHeight + (2*marg)));
+bubbles = colno*rowno;
+
+reset(bubbles);
+
+// Reload the page on 0.3 sec after user stops resizing
+let resizeTimer;
+window.addEventListener("resize", () => {
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {location.reload()} , 300); // Adjust delay as needed
+});
